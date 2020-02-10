c4f75a2b4d471932bc309c7b32fe3bcdfb936cd5
Corey Tan <coreyct@uci.edu>
2020-02-09 23:06:19 -0800

    change

--- scraper.py
+++ scraper.py
@@ -13,8 +13,9 @@ def extract_next_links(mem, url, resp):
     print("resp .url: ", resp.url)
     print("resp status: ", resp.status)
     print("resp error: ", resp.error)
-    print("resp text: ", resp.text)
-    print("resp json: ", resp.json())
+    if resp.status == 200:
+	    print("resp text: ", resp.text)
+	    print("resp json: ", resp.json())
     #if url not in mem:
     	#find the hrefs.
     return lst
