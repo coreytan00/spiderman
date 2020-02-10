ed4752244cdf317c9223451c16e02b6acb9b6d46
Corey Tan <coreyct@uci.edu>
2020-02-10 00:09:32 -0800

    change

--- scraper.py
+++ scraper.py
@@ -18,10 +18,10 @@ def extract_next_links(mem, url, resp):
 	   	soup = BeautifulSoup(html_doc, 'html.parser')
 	   	for link in soup.find_all('a'):
 	   		hlink = link.get('href')
+	   		#check if in cache
 	   		if hlink not in mem:
 	   			mem.add(hlink)
 	   			lst.append(hlink)
-	   	print(lst)
     return lst
     # defend our position of low quality urls.
 
@@ -31,9 +31,10 @@ def extract_next_links(mem, url, resp):
 def is_valid(url):
     try:
         parsed = urlparse(url)
+        print(parsed)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
+        return (not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -41,18 +42,24 @@ def is_valid(url):
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
-
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()))
+        """
+        	and
+        	#check if in domain
+        	re.match("ics.uci.edu")
+        	or
+        	re.match("cs.uci.edu")
+        """
     except TypeError:
         print ("TypeError for ", parsed)
         raise
 
 DOMAINS = [
-	".ics.uci.edu/",
-	".cs.uci.edu/",
-	".informatics.uci.edu/",
-	".stat.uci.edu/",
-	"today.uci.edu/department/information_computer_sciences/"
+	"https://www.ics.uci.edu/",
+	"https://www.cs.uci.edu/",
+	"https://www.informatics.uci.edu/",
+	"https://www.stat.uci.edu/",
+	"https://www.today.uci.edu/department/information_computer_sciences/"
 ]
 
 STOPWORDS = [
