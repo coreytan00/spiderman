b4fc22d432db1f38588716b14d335c2019d31448
Corey Tan <coreyct@uci.edu>
2020-02-09 22:32:55 -0800

    change

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ def scraper(mem, url, resp):
     return [link for link in links if is_valid(link)] #will be thrown in frontier by worker
 
 def extract_next_links(mem, url, resp):
+	print('runs')
     # Implementation requred.
     # go through find all the urlus.
     if url not in mem:
