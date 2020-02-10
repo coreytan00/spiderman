8576ff2f3802b2e4ee903513fe31a712153669ca
Corey Tan <polarbearlu968@gmail.com>
2020-02-09 18:06:57 -0800

    hah

--- launch.py
+++ launch.py
@@ -12,11 +12,11 @@ def main(config_file, restart):
     config = Config(cparser)
     config.cache_server = get_cache_server(config, restart)
     crawler = Crawler(config, restart)
+    print("runs here")
     crawler.start()
 
 
 if __name__ == "__main__":
-    print("ran")
     parser = ArgumentParser()
     parser.add_argument("--restart", action="store_true", default=False)
     parser.add_argument("--config_file", type=str, default="config.ini")
