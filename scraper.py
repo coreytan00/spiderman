f8c039afe6ac5ebbf3f5a459a2cfeccb4481a1c5
Corey Tan <coreyct@uci.edu>
2020-02-11 23:58:20 -0800

    change

--- scraper.py
+++ scraper.py
@@ -58,7 +58,7 @@ def is_valid(config, robot_cache, robot_url_cache, mem, url):
 						robot_txt = robot_resp.raw_response.text
 						robot_txt2 = robot_resp.raw_response.content
 						for line in robot_txt:
-							line = line.rstrip("\n")
+							#line = line.rstrip("\n")
 							print(line)
 						#findall
 
