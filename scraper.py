94a662aac64287c5e934405db3b11e8a85584b98
Corey Tan <coreyct@uci.edu>
2020-02-12 19:35:21 -0800

    chg

--- scraper.py
+++ scraper.py
@@ -119,7 +119,7 @@ def parse(parsed, robot_txt, robot_cache_a, robot_cache_d):
 				if key == "disallow":
 					robot_cache_d.add(parsed.scheme + "://" + parsed.netloc + value)
 				elif key == "allow":
-	            	robot_cache_a.add(parsed.scheme + "://" + parsed.netloc + value)
+					robot_cache_a.add(parsed.scheme + "://" + parsed.netloc + value)
 	            """
 	            elif key == "crawl-delay":
 	            	if value.strip().isdigit():
