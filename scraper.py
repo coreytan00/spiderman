efc553014c1fae5f99488d017d7b957b0654f7dd
Corey Tan <coreyct@uci.edu>
2020-02-12 22:01:48 -0800

    change

--- scraper.py
+++ scraper.py
@@ -105,20 +105,20 @@ def is_valid(config, robot_cache_a, robot_cache_d, robot_url_cache, mem, url, re
 				#found in robot_url_cache - just means it's been checked.
 				#doesn't necessarily mean there is a robots.txt
 				if url not in mem:
-					#simhash
+					#simhash here
 					index=SimhashIndex(mem,k=10)
 					s = Simhash(get_features(resp.raw_response.text))
 					if index.get_near_dups(s) != []:
 						return False
 					else:
 						if url in robot_cache_a:
+							print("URL ADDED:", url)
 							mem[str(url)] = s
-							print(URL ADDED: url)
 							return True
 						elif url in robot_cache_d:
 							return False
 						else:
-							print(URL ADDED: url)
+							print("URL ADDED:", url)
 							mem[str(url)] = s
 							return True
 				else:
